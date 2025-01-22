9f4eebfd2aaf95358e02f07001d65007b7bbe6fb
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-01-22 08:07:44 +0530

    pattern4

---
+++ pattern4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the n";
+    cin>>n;
+    for(int i =1;i <=n; i++){
+        int k = n-i+1;
+        for(int j =1; j <= k ; j++ ){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
