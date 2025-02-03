35222a663ab79bc18f264e0a039b2c53452fbeae
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-04 02:42:19 +0530

    another_num_print

---
+++ pattern9.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n\n";
+    cin>>n;
+    for(int i =1 ; i<=n;i++){
+        for(int j =n; j>=1 ;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+// enter the value of n4
+// 4321
+// 4321
+// 4321
+// 4321
