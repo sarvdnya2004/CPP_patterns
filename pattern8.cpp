8a0d733d5e47a64381f4a73d9ae72da08c299f8e
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-04 02:27:23 +0530

    num_pattern

---
+++ pattern8.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n\n";
+    cin>>n;
+    for(int i =1 ; i<=n;i++){
+        for(int j =1; j<=n ;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+// enter the value of n
+// 4
+// 1234
+// 1234
+// 1234
+// 1234
