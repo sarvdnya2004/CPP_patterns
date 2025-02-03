04d35976da40039a5415cf120f6d03d7b3355e89
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-03 16:44:25 +0530

    3 new patterns

---
+++ pattern6.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i = 1; i<=3;i++){
+        for(int j = 1; j <=3; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+}
+
+// output
+// 111
+// 222
+// 333
