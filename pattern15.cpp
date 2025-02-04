b34c5e0d728c065729d61c085837a08b8a150ba4
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-04 16:36:15 +0530

    alphabet pattern

---
+++ pattern15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    for(int i = 1; i<= n; i++){
+        for (int j = 1; j < n; j++){
+            char k = 96 + i;
+            cout<<k;
+        }
+        cout<<endl;
+    }
+}
+
+// enter the value of n4
+// aaa
+// bbb
+// ccc
+// ddd
