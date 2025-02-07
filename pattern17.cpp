0aa05510907f04c41fcade2bc2a31f6f6a68c635
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-07 12:20:50 +0530

    new pattern

---
+++ pattern17.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    char k = 65;
+    for(int i =1 ; i <= n; i++){
+        
+        for(int j = 1; j <=n;j++){
+            
+            cout<<k;
+            k += 1;
+        }
+        
+        cout<<endl;
+    }
+}
+
+// o/p
+// ABCD
+// EFGH
+// IJKL
+// MNOP
